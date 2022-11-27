bf26ecd98f1f789a13e506e892a79b706d3c4542
KGRANGER <kevin.granger@ynov.com>
2022-11-27 22:24:45 +0100

    Player Animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,23 +15,54 @@ public class PlayerController : MonoBehaviour
     public float damage = 10f;
     public float range = 1f;
     private float _cd;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         if (Input.GetKey(GameManager.gm.Forward))
         {
             transform.position += Vector3.up * (Time.deltaTime * speed);
+            _animator.SetBool("UpKey", true);
         }
         if (Input.GetKey(GameManager.gm.Backward))
         {
             transform.position += Vector3.down * (Time.deltaTime * speed);
+            _animator.SetBool("DownKey", true);
         }
         if (Input.GetKey(GameManager.gm.Right))
         {
             transform.position += Vector3.right * (Time.deltaTime * speed);
+            _animator.SetBool("RightKey", true);
         }
         if (Input.GetKey(GameManager.gm.Left))
         {
             transform.position += Vector3.left * (Time.deltaTime * speed);
+            _animator.SetBool("LeftKey", true);
+        }
+
+        if (_animator != null)
+        {
+            if (Input.GetKeyUp(GameManager.gm.Forward))
+            {
+                _animator.SetBool("UpKey", false);
+            }
+            if (Input.GetKeyUp(GameManager.gm.Backward))
+            {
+                _animator.SetBool("DownKey", false);
+            }
+            if (Input.GetKeyUp(GameManager.gm.Right))
+            {
+                _animator.SetBool("RightKey", false);
+            }
+            if (Input.GetKeyUp(GameManager.gm.Left))
+            {
+                _animator.SetBool("LeftKey", false);
+            }
         }
 
         if (_cd <= Time.time)
